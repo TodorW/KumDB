87d3d3e1b64309ac32bbd0cb8c3545403bdc39c0
Vuk Todorović <vuk.todorovic@protonmail.com>
2025-05-30 00:27:37 +0200

    meep

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="kumdb",
+    version="3.0.0",
+    packages=find_packages(),
+    install_requires=[],  # Add dependencies if you have any
+    python_requires=">=3.7",
+)
