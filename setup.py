9a84706e8bd288797eb2ee559705f978fe4891be
Vuk Todorović <vuk.todorovic@protonmail.com>
2025-05-30 01:16:03 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,25 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="kumdb",
-    version="3.0.0",
+    name='KumDB',
+    version='3.0.0',
+    author='Vuk Todorovic',
+    author_email='vuk.todorovic@protonmail.com',
+    description='A custom file-based DB system with JSON, CSV, msgpack support',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/TodorW/KumDB',
     packages=find_packages(),
-    install_requires=[],  # Add dependencies if you have any
-    python_requires=">=3.7",
+    python_requires='>=3.7',
+    install_requires=[
+    'msgpack>=1.0.0',
+    'typing_extensions>=4.0.0',
+    'cryptography>=3.4',
+    'dataclasses; python_version < "3.7"',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: OS Independent',
+    ],
 )
